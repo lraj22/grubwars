9dc84f4154a7d978a7fe04f48d66fdefd41847f5
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-07-13 20:08:52 +0530

    Socket mode logging

--- client.js
+++ client.js
@@ -4,15 +4,18 @@ const { App } = bolt;
 
 const startTime = Date.now();
 
+const isSocketMode = (process.env.GRUBWARS_SOCKET_MODE === "true"); // only true in development
 const app = new App({
 	"token": process.env.GRUBWARS_BOT_TOKEN,
 	"signingSecret": process.env.GRUBWARS_SIGNING_SECRET,
-	"socketMode": process.env.GRUBWARS_SOCKET_MODE === "true", // only true in development
+	"socketMode": isSocketMode,
 	"appToken": process.env.GRUBWARS_APP_TOKEN,
 	"endpoints": {
 		"events": "/slack/grubwars/events",
 	},
 });
+
+log(isSocketMode ? "Starting in Socket Mode!" : "Starting in Request URL Mode!");
 
 await app.start(process.env.GRUBWARS_PORT || 5040);
 log(`⚡ Slack bot ready in ${Date.now() - startTime}ms.`);
